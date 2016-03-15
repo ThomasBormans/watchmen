237d6978076ae569f0cd5ce06d07878a11a95c7f
Glenn Geenen <Glenn Geenen>
2016-03-15 17:27:20 +0100

    add mongo storage

--- config/storage.js
+++ config/storage.js
@@ -7,6 +7,9 @@ module.exports = {
         port: process.env.WATCHMEN_REDIS_PORT_PRODUCTION || 1216,
         host: process.env.WATCHMEN_REDIS_ADDR_PRODUCTION || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_PRODUCTION || 1
+      },
+      'mongo' : {
+        url: process.env.WATCHMEN_MONGO_URL_PRODUCTION || 'mongodb://localhost:27017/watchmen'
       }
     }
   },
@@ -18,6 +21,9 @@ module.exports = {
         port: process.env.WATCHMEN_REDIS_PORT_DEVELOPMENT || 1216,
         host: process.env.WATCHMEN_REDIS_ADDR_DEVELOPMENT || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_DEVELOPMENT || 2
+      },
+      'mongo' : {
+        url: process.env.WATCHMEN_MONGO_URL_DEVELOPMENT || 'mongodb://localhost:27017/watchmen'
       }
     }
   },
@@ -29,6 +35,9 @@ module.exports = {
         port: process.env.WATCHMEN_REDIS_PORT_TEST || 6666,
         host: process.env.WATCHMEN_REDIS_ADDR_TEST || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_TEST || 1
+      },
+      'mongo' : {
+        url: process.env.WATCHMEN_MONGO_URL_TEST || 'mongodb://localhost:27017/watchmen-test'
       }
     }
   }
